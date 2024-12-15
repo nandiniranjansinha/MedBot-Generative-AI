0ddf83ef2d75f985c393062056d6cb0873ac2ddd
Nandini <nandirsinha@gmail.com>
2024-12-16 01:33:44 +0530

    folder structure added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MedBot-Generative-AI
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Nandini',
+    author_email= 'nandirsinha@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
